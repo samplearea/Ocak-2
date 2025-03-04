48de9ddcc229dd6726ede0b42b06c48eefa7c79f
Sefa <yusufsefayetkin@gmail.com>
2025-03-05 01:30:31 +0300

    Ocak 2
    Net React Ödev - Ocak 2

---
+++ Net React Ödev  Ocak 2/Net React Ödev  Ocak 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Kullanıcıdan ilk sayıyı al
+        Console.Write("Birinci sayıyı girin: ");
+        double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+        // Kullanıcıdan ikinci sayıyı al
+        Console.Write("İkinci sayıyı girin: ");
+        double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+        // İki sayının toplamını hesapla
+        double toplam = sayi1 + sayi2;
+
+        // Sonucu ekrana yazdır
+        Console.WriteLine("Toplam: " + toplam);
+    }
+}
